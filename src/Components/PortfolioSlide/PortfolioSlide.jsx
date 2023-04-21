ba94fcd4dc2643c3c075152a42a52e11d21c57d1
Eduard <blademan82@gmail.com>
2023-04-21 04:07:13 +0300

    add new projects

--- src/Components/PortfolioSlide/PortfolioSlide.jsx
+++ src/Components/PortfolioSlide/PortfolioSlide.jsx
@@ -1,18 +1,23 @@
-import Button from '../../Components/Button/Button';
-
 export default function PortfolioSlide(props) {
-  return (
-    <>
-      <div className="flex flex-col justify-center pt-12 items-center ">
-        <div className="flex flex-col md:flex-row space-y-3 md:space-x-10 px-4 pb-20">
-          <img src={props.img} alt="Lake" className="max-w-sm item-center" />
-          <div className="flex flex-col pt-6">
-            <h3 className="dark:text-D_titleColor font-bold text-left text-2xl">{props.title}</h3>
-            <p className="dark:text-D_textColor text-textColor pb-6 pt-2">{props.description}</p>
-            <Button name="Link" href={props.link} icon="uil uil-download-alt " />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='flex flex-col justify-center pt-12 items-center '>
+				<div className='flex flex-col md:flex-row space-y-3 md:space-x-10 px-4 pb-20'>
+					<img src={props.img} alt='Lake' className='max-w-xs item-center aspect-[4/3]' />
+					<div className='flex flex-col pt-6'>
+						<h3 className='dark:text-D_titleColor font-bold text-left text-2xl'>{props.title}</h3>
+						<p className='dark:text-D_textColor text-textColor pb-6 pt-2'>{props.description}</p>
+						<a
+							className='flex justify-between space-x-2 mt-8 w-48 bg-firstColor px-4 py-4 font-medium rounded-md text-white hover:bg-firstColorAlt cursor-pointer'
+							name='Link'
+							href={props.link}
+							icon='uil uil-download-alt '
+						>
+							Link <i className='uil uil-message button__icon'></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
--- src/Utils/Data/PotfolioData.js
+++ src/Utils/Data/PotfolioData.js
@@ -1,26 +1,40 @@
-import slide1 from '../../Assets/img/portfolio/Snapcar.webp';
-import slide2 from '../../Assets/img/portfolio/guaerdev.webp';
-import slide3 from '../../Assets/img/portfolio/transportor.webp';
+import slide4 from '../../Assets/img/portfolio/Snapcar.webp'
+import slide1 from '../../Assets/img/portfolio/crypto.png'
+import slide2 from '../../Assets/img/portfolio/guaerdev.webp'
+import slide5 from '../../Assets/img/portfolio/portfolio.webp'
+import slide3 from '../../Assets/img/portfolio/transportor.webp'
 
 const portfolio_db = [
-  {
-    img: slide1,
-    title: 'Snap Car Rent',
-    description: 'Wordpress project',
-    link: 'https://snapcarrent.com/',
-  },
-  {
-    img: slide2,
-    title: 'Guardev',
-    description: 'Wordpress project - Elementor, Php, JavaScript, custom CSS',
-    link: 'http://guardev.com/',
-  },
-  {
-    img: slide3,
-    title: 'Transportor',
-    description: 'Wordpress project - Elementor, Php, JavaScript, custom CSS',
-    link: 'https://www.transportor.com/',
-  },
-];
+	{
+		img: slide1,
+		title: 'Crypto Currency',
+		description: 'React project',
+		link: 'https://crypto-price-react-t.netlify.app/',
+	},
+	{
+		img: slide5,
+		title: 'Portfolio Theme',
+		description: 'React project',
+		link: 'https://frontpx2023.netlify.app/',
+	},
+	{
+		img: slide4,
+		title: 'Snap Car Rent',
+		description: 'Wordpress project',
+		link: 'https://snapcarrent.com/',
+	},
+	{
+		img: slide2,
+		title: 'Guardev',
+		description: 'Wordpress project',
+		link: 'http://guardev.com/',
+	},
+	{
+		img: slide3,
+		title: 'Transportor',
+		description: 'Wordpress project',
+		link: 'https://www.transportor.com/',
+	},
+]
 
-export default portfolio_db;
+export default portfolio_db
